724ba47025e111dad32b3368631eb1fe03c1115e
Ashley Brooks <ashley.brooks.a@gmail.com>
2017-04-19 21:36:32 -0700

    Added PagerDuty integration. Not working yet #TODO

--- server.py
+++ server.py
@@ -44,6 +44,44 @@ def complete_todo():
     return redirect('/')
 
 
+@app.route('/error', methods=['POST'])
+def page_pagerduty():
+    """Send JSON body to PagerDuty."""
+
+    # TODO - Fix
+
+    url = 'https://events.pagerduty.com/generic/2010-04-15/create_event.json'
+
+    payload = {    
+      "service_key": "b3afd708e7984831b9abaecff56995b6", # for learning purposes so not putting this in enviro variable
+      "event_type": "trigger",
+      "description": "FAILURE for production/HTTP on machine srv01.acme.com",
+      "client": "Sample Monitoring Service",
+      "client_url": "https://monitoring.service.com",
+      "details": {
+        "ping time": "1500ms",
+        "load avg": 0.75
+      },
+      "contexts":[ 
+        {
+          "type": "link",
+          "href": "http://acme.pagerduty.com"
+        },{
+          "type": "link",
+          "href": "http://acme.pagerduty.com",
+          "text": "View the incident on PagerDuty"
+        },{
+          "type": "image",
+          "src": "https://chart.googleapis.com/chart?chs=600x400&chd=t:6,2,9,5,2,5,7,4,8,2,1&cht=lc&chds=a&chxt=y&chm=D,0033FF,0,0,5,1"
+        }
+      ]
+    }
+
+    r = requests.post(url, data=json.dumps(payload))
+
+    print r.json()
+
+
 if __name__ == "__main__":
     app.debug = True
 
