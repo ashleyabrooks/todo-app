b9cced9bdc66c25e9fe3159405be3365832cc5d3
Ashley Brooks <ashley.brooks.a@gmail.com>
2017-04-19 19:53:46 -0700

    Changed data structure to dict instead of array

--- server.py
+++ server.py
@@ -2,13 +2,20 @@ from flask import Flask, request, render_template, redirect
 
 app = Flask(__name__)
 
-todos = []
+todos = {}
 
 @app.route('/')
 def show_homepage():
     """Display homepage where user can view all of their todos."""
 
-    return render_template('index.html', todos=todos)
+    # filter dictionary by completion status so user only sees incomplete todos
+    incomplete_todos = {}
+
+    for todo in todos:
+        if todos[todo] == 'incomplete':
+            incomplete_todos[todo] = 'incomplete'
+
+    return render_template('index.html', todos=incomplete_todos)
 
 
 @app.route('/create-todo', methods=['POST'])
@@ -17,29 +24,23 @@ def create_todo():
 
     todo = request.form.get('todo')
 
-    # if todos == {}:
-    #     todo_index = 0
-    # else:
-    #     todo_index = 1 + max(todos.keys())
-
-    # todos[todo_index] = todo
-
-    todos.append(todo)
+    if todo in todos:
+        pass
+    else:
+        todos[todo] = 'incomplete'
 
     return redirect('/')
 
-@app.route('/update-todo', methods=['PATCH'])
-def update_todo():
-    """Update todo."""
 
-    pass
+@app.route('/complete-todo', methods=['POST'])
+def complete_todo():
+    """Complete todo selected by user."""
 
-@app.route('/delete-todo', methods=['DELETE'])
-def delete_todo():
-    """Delete todo selected by user."""
+    todo = request.form.get('todo')
 
-    todo = Todo.query.get(id)
+    todos[todo] = 'complete'
 
+    return redirect('/')
 
 
 if __name__ == "__main__":
